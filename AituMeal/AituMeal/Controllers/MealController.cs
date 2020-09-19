57863f55f389a19b928e0cd0d9ae08526e43388b
aitinsan <57362967+aitinsan@users.noreply.github.com>
2020-09-19 20:06:08 +0600

    Add files via upload

---
+++ AituMeal/AituMeal/Controllers/MealController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AituMeal.Models;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Assigment1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class LessonController : ControllerBase
+    {
+        private List<Meal> _meal = new List<Meal>()
+        {
+            new Meal(){MealId=1,MealName="оливье",MealPrice=150, MealWeight=100, MealType = "Салат", MealDate=new DateTime(2020,07,16)  },
+            new Meal(){MealId=1,MealName="капустный",MealPrice=80, MealWeight=100, MealType = "Салат", MealDate=new DateTime(2020,07,16)  },
+            new Meal(){MealId=1,MealName="итальянский",MealPrice=150, MealWeight=100, MealType = "Салат", MealDate=new DateTime(2020,07,16)  },
+            new Meal(){MealId=1,MealName="из свеклы с сыром",MealPrice=120, MealWeight=100, MealType = "Салат", MealDate=new DateTime(2020,08,16)  },
+            new Meal(){MealId=1,MealName="из цветочной капусты",MealPrice=120, MealWeight=100, MealType = "Салат", MealDate=new DateTime(2020,08,16)  },
+            new Meal(){MealId=1,MealName="греческий",MealPrice=150, MealWeight=100, MealType = "Салат", MealDate=new DateTime(2020,08,16)  },
+            new Meal(){MealId=1,MealName="морковный",MealPrice=80, MealWeight=100, MealType = "Салат", MealDate=new DateTime(2020,08,16)  },
+            new Meal(){MealId=1,MealName="из фунчезы с овощями",MealPrice=120, MealWeight=100, MealType = "Салат", MealDate=new DateTime(2020,09,16)  },
+            new Meal(){MealId=1,MealName="нежность",MealPrice=150, MealWeight=100, MealType = "Салат", MealDate=new DateTime(2020,09,16)  },
+            new Meal(){MealId=1,MealName="Витаминный",MealPrice=120, MealWeight=100, MealType = "Салат", MealDate=new DateTime(2020,10,16)  },
+            new Meal(){MealId=1,MealName="Малибу",MealPrice=150, MealWeight=100, MealType = "Салат", MealDate=new DateTime(2020,10,16)  },
+        };
+    
+        [HttpGet]
+        public List<Meal> GetAllMeal()
+        {
+            return _meal.OrderBy(x => x.MealId).ToList();
+        }
+        [HttpGet("get/byname/{name}")]
+        public List<Meal> GetAllLessonsByName(string name)
+        {
+            return _meal.Where(c => c.MealName.Equals(name)).OrderBy(x => x.MealId).ToList();
+        }
+        [HttpGet("get/bytype/{mealtype}")]
+        public List<Meal> GetAllLessonsByTeacher(string mealtype)
+        {
+            return _meal.Where(c => c.MealType.Equals(mealtype)).OrderBy(x => x.MealId).ToList();
+        }
+        
+
+
+
+
+
+
+    }
+}
---
+++ AituMeal/AituMeal/Models/Meal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AituMeal.Models
+{
+    public class Meal
+    {
+        public int MealId { get; set; }
+        public string MealName { get; set; }
+        public int MealPrice { get; set; }
+        public int MealWeight { get; set; }
+        public string MealType { get; set; }
+        public DateTime MealDate { get; set; }
+    }
+}
